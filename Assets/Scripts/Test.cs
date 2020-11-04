03383808efdfbf9f0bc23689d6a3fa5faeee1a12
FirepadCN <1768151377@qq.com>
2020-11-04 22:19:44 +0800

    :tada: 冰面裂痕、视角星空、病毒突触

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+#warning "这个地方后续需要优化"
+        //TODO:这里以后需要处理0
+#pragma warning disable 1030
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
